9ffb587dabe6dc4aad547ed675ce4596bcb53224
Fussek Aziz <develop@doors.vision>
2023-05-29 23:10:28 +0300

    added login, logout and authCheck

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5adecfbd3efd4a2b"; }
+/******/ 		__webpack_require__.h = function() { return "9eebfaf66523087b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/.next/static/webpack/webpack.a36bdd6deed817f6.hot-update.js
+++ client/.next/static/webpack/webpack.a36bdd6deed817f6.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a79c723db436a850"; }
+/******/ 	__webpack_require__.h = function() { return "ce8eb0963b16504c"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
